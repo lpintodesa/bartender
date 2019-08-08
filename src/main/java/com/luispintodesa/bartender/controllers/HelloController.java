fd2bb725c5061cb371feb46eddf5331822f8599f
lpintodesa <luis.pintodesa@gmail.com>
2019-08-08 17:37:34 -0500

    Landing Page View completed (with text hardcoded into the HTML)

--- src/main/java/com/luispintodesa/bartender/controllers/HelloController.java
+++ src/main/java/com/luispintodesa/bartender/controllers/HelloController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     @RequestMapping(value = "")
-    @ResponseBody
     public String index() {
-        return "Hello World";
+        return "index";
     }
 }
