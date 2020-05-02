7d9cfac29acdc14ddf6267aafa6085fddb5e0732
lpintodesa <luis.pintodesa@gmail.com>
2020-05-02 18:59:37 -0500

    Refactored WhatCanIMakeController

--- src/main/java/com/luispintodesa/bartender/controllers/WhatCanIMakeController.java
+++ src/main/java/com/luispintodesa/bartender/controllers/WhatCanIMakeController.java
@@ -1,35 +1,37 @@
 package com.luispintodesa.bartender.controllers;
 
-import com.luispintodesa.bartender.models.Deserializer;
+import com.luispintodesa.bartender.models.DeserializerUtils;
 import com.luispintodesa.bartender.models.Drink;
-import com.luispintodesa.bartender.models.IngredientToDrinks;
+import com.luispintodesa.bartender.models.IngredientToDrinksUtils;
 import com.luispintodesa.bartender.models.User;
-import com.luispintodesa.bartender.models.dao.UserDao;
 import com.luispintodesa.bartender.models.forms.WhatCanIMakeForm;
 import com.luispintodesa.bartender.models.manipulation.DrinkListDivider;
 import com.luispintodesa.bartender.models.manipulation.SpaceToUnderscoreConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
-import static com.luispintodesa.bartender.models.Constants.*;
+import static com.luispintodesa.bartender.models.Constants.NO_RESULTS;
+import static com.luispintodesa.bartender.models.Constants.NO_RESULTS_TEMPLATE;
+import static com.luispintodesa.bartender.models.Constants.RESULTS_TEMPLATE;
+import static com.luispintodesa.bartender.models.Constants.SEARCH_RESULTS;
+import static com.luispintodesa.bartender.models.Constants.TITLE;
 
 @Controller
 @RequestMapping("")
 public class WhatCanIMakeController extends AbstractController {
 
-    @Autowired
-    private UserDao userDao;
-
     @GetMapping(value = "whatcanimake")
     public String myBarForm(Model model) {
         model.addAttribute(new WhatCanIMakeForm());
-        model.addAttribute("ingredients", Deserializer.listAllIngredients());
+        model.addAttribute("ingredients", DeserializerUtils.listAllIngredients());
         model.addAttribute(TITLE, "What Can I Make?");
         return "whatcanimake";
     }
@@ -40,12 +42,12 @@ public class WhatCanIMakeController extends AbstractController {
         String cocktailName = SpaceToUnderscoreConverter.convert(form.getCocktailName());
 
 
-        if (Deserializer.searchDrinkByName(cocktailName)==null){
+        if (DeserializerUtils.searchDrinkByName(cocktailName)==null){
             model.addAttribute(TITLE, NO_RESULTS);
             return NO_RESULTS_TEMPLATE;
         }
 
-        List<Drink> drinks = (ArrayList<Drink>) Deserializer.searchDrinkByName(cocktailName);
+        List<Drink> drinks = DeserializerUtils.searchDrinkByName(cocktailName);
 
         List<List<Drink>> lists = DrinkListDivider.divideInThree(drinks);
         List<Drink> one = lists.get(0);
@@ -60,47 +62,27 @@ public class WhatCanIMakeController extends AbstractController {
     }
 
     @PostMapping(value = "whatcanimake/surpriseme")
-    public String surpriseMe (Model model, HttpServletRequest request){
+    public String surpriseMe (Model model, HttpServletRequest request, @ModelAttribute @RequestParam(required=true) int numberOfMissingIngredients){
 
         User theUser = getUserFromSession(request.getSession());
 
-        List<Drink> drinks = IngredientToDrinks.idsToDrinks(IngredientToDrinks.addDrinkIDsToList(IngredientToDrinks.ingredientSearch(theUser)));
+        List<Drink> drinks = IngredientToDrinksUtils.userIngredientsToDrinks(theUser,numberOfMissingIngredients);
 
-        IngredientToDrinks.setMatchCounter(drinks, theUser);
-
-        List<List<Drink>> scoreList = DrinkListDivider.divideByScore(drinks);
-
-        List<List<Drink>> score0 = DrinkListDivider.divideInThree(scoreList.get(0));
-        List<List<Drink>> score1 = DrinkListDivider.divideInThree(scoreList.get(1));
-        List<List<Drink>> score2 = DrinkListDivider.divideInThree(scoreList.get(2));
-
-        if (scoreList.get(0).isEmpty()&& scoreList.get(1).isEmpty()&&scoreList.get(2).isEmpty()){
+        if (drinks.isEmpty()){
             model.addAttribute(TITLE, NO_RESULTS);
             return NO_RESULTS_TEMPLATE;
         }
-        List<Drink> score0one = score0.get(0);
-        List<Drink> score0two = score0.get(1);
-        List<Drink> score0three = score0.get(2);
 
-        List<Drink> score1one = score1.get(0);
-        List<Drink> score1two = score1.get(1);
-        List<Drink> score1three = score1.get(2);
+        List<List<Drink>> listOfListsOfDrinks = DrinkListDivider.divideInThree(drinks);
 
-        List<Drink> score2one = score2.get(0);
-        List<Drink> score2two = score2.get(1);
-        List<Drink> score2three = score2.get(2);
+        List<Drink> firstColumn = listOfListsOfDrinks.get(0);
+        List<Drink> secondColumn = listOfListsOfDrinks.get(1);
+        List<Drink> thirdColumn = listOfListsOfDrinks.get(2);
 
-        model.addAttribute("score0one", score0one);
-        model.addAttribute("score0two", score0two);
-        model.addAttribute("score0three", score0three);
-
-        model.addAttribute("score1one", score1one);
-        model.addAttribute("score1two", score1two);
-        model.addAttribute("score1three", score1three);
-
-        model.addAttribute("score2one", score2one);
-        model.addAttribute("score2two", score2two);
-        model.addAttribute("score2three", score2three);
+        model.addAttribute("firstColumn", firstColumn);
+        model.addAttribute("secondColumn", secondColumn);
+        model.addAttribute("thirdColumn", thirdColumn);
+        model.addAttribute("score", numberOfMissingIngredients);
 
         model.addAttribute(TITLE, SEARCH_RESULTS);
 
@@ -125,12 +107,12 @@ public class WhatCanIMakeController extends AbstractController {
             }
         }
 
-        if (Deserializer.searchDrinkByMultipleIngredients(search.toString())==""){
+        if (DeserializerUtils.searchDrinkByMultipleIngredients(search.toString()).isEmpty()){
             model.addAttribute(TITLE, NO_RESULTS);
             return NO_RESULTS_TEMPLATE;
         }
 
-        List<Drink> drinks = (ArrayList<Drink>) Deserializer.searchDrinkByMultipleIngredients(search.toString());
+        List<Drink> drinks = DeserializerUtils.searchDrinkByMultipleIngredients(search.toString());
         List<List<Drink>> lists = DrinkListDivider.divideInThree(drinks);
         List<Drink> one = lists.get(0);
         List<Drink> two = lists.get(1);
