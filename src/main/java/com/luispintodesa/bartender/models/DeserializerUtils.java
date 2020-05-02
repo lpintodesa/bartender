6f1ba3271a930ee1737937cd6f04f0419f28d47d
lpintodesa <luis.pintodesa@gmail.com>
2020-05-02 18:57:24 -0500

    Significantly refactored DeserializerUtils to reduce code duplication.

--- src/main/java/com/luispintodesa/bartender/models/DeserializerUtils.java
+++ src/main/java/com/luispintodesa/bartender/models/DeserializerUtils.java
@@ -1,9 +1,11 @@
 package com.luispintodesa.bartender.models;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luispintodesa.bartender.models.manipulation.SpaceToUnderscoreConverter;
+import com.luispintodesa.bartender.models.wrappers.DrinkWrapper;
+import com.luispintodesa.bartender.models.wrappers.IngredientInListWrapper;
+import com.luispintodesa.bartender.models.wrappers.IngredientWrapper;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.BufferedReader;
@@ -14,7 +16,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Deserializer {
+public class DeserializerUtils {
 
     //TODO: Explore using ObjectReader.
     private static final ObjectMapper MAPPER = new ObjectMapper();
@@ -22,7 +24,7 @@ public class Deserializer {
     // TODO: Unwrap root using UNWRAP_ROOT_VALUE annotation to improve performance, elegance and dispense with superfluous wrapper classes.
     static {
         MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        MAPPER.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);}
+    }
 
     private static final String URL_FIRST_HALF="https://www.thecocktaildb.com/api/json/v2/";
     
@@ -46,63 +48,43 @@ public class Deserializer {
         return "";
     }
 
-    public static Object listAllIngredients() {
+    public static List<IngredientInList> listAllIngredients() {
+
+        List<IngredientInList> allIngredients = new ArrayList<>();
+
         try {
             URL ingredientList = new URL(URL_FIRST_HALF+getKey()+"/list.php?i=list");
-            List<IngredientInList> listIngredients= MAPPER.readValue(ingredientList, new TypeReference<List<IngredientInList>>(){});
-            return listIngredients;
+            IngredientInListWrapper listIngredients= MAPPER.readValue(ingredientList, IngredientInListWrapper.class);
+            allIngredients=listIngredients.getDrinks();
         } catch (IOException e){
-            e.printStackTrace();
+            allIngredients.clear();
         }
-        return "";
+        return allIngredients;
     }
 
-    public static Object searchDrinkById(Integer id) {
+    public static Drink searchDrinkById(Integer id) {
 
-        String strId = String.valueOf(id);
-
-        try {
-            URL drinkList = new URL(URL_FIRST_HALF+getKey()+"/lookup.php?i=" + strId);
-            List<Drink> drinks = MAPPER.readValue(drinkList, new TypeReference<List<Drink>>(){});
-            return drinks.get(0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "";
+        return searchDrinks(URL_FIRST_HALF+getKey()+"/lookup.php?i="+id).get(0);
     }
 
-    public static Object searchDrinkByMultipleIngredients(String name) {
+    public static List<Drink> searchDrinkByMultipleIngredients(String name) {
 
-        try {
-            URL drinkList = new URL(URL_FIRST_HALF+getKey()+"/filter.php?i="+ name);
-            List<Drink> drinks = MAPPER.readValue(drinkList, new TypeReference<List<Drink>>(){});
-            return drinks;
-
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        return "";
+        return searchDrinks(URL_FIRST_HALF+getKey()+"/filter.php?i="+ name);
     }
 
-    public static Object searchDrinkByName(String name) {
+    public static List<Drink> searchDrinkByName(String name) {
 
-        try {
-            URL drinkList = new URL(URL_FIRST_HALF+getKey()+"/search.php?s="+ name);
-            List<Drink> drinks = MAPPER.readValue(drinkList, new TypeReference<List<Drink>>(){});
-            return drinks;
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        return "";
+        return searchDrinks(URL_FIRST_HALF+getKey()+"/search.php?s="+ name);
     }
 
-    public static Object searchDrinkBySingleIngredient(String url) {
+    public static List<Drink> searchDrinks(String url) {
 
         List<Drink> drinks = new ArrayList<>();
 
         try {
-            URL drinkIDs = new URL (url);
-            drinks = MAPPER.readValue(drinkIDs, new TypeReference<List<Drink>>(){});
+            URL drinkSearchURL = new URL (url);
+            DrinkWrapper drinkWrapper = MAPPER.readValue(drinkSearchURL, DrinkWrapper.class);
+            drinks = drinkWrapper.getDrinks();
 
         } catch (IOException e) {
             drinks.clear();
@@ -110,15 +92,21 @@ public class Deserializer {
         return drinks;
     }
 
-    public static Object searchIngredientByName(String name) {
+    public static Ingredient searchIngredientByName(String name) {
+
+        List<Ingredient> ingredients = new ArrayList<>();
 
         try {
             URL ingredientList = new URL(URL_FIRST_HALF+getKey()+"/search.php?i="+ SpaceToUnderscoreConverter.convert((name)));
-            List<Ingredient> ingredients=MAPPER.readValue(ingredientList, new TypeReference<List<Ingredient>>(){});
-            return ingredients.get(0);
+            IngredientWrapper ingredientWrapper= MAPPER.readValue(ingredientList, IngredientWrapper.class);
+            ingredients=ingredientWrapper.getIngredient();
+
         } catch (IOException e){
-            e.printStackTrace();
+            ingredients.clear();
         }
-        return "";
+        return ingredients.get(0);
+    }
+
+    private DeserializerUtils() {
     }
 }
