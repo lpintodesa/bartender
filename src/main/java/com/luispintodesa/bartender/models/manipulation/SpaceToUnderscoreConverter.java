52ea3e8126fdd83c0b91f24d54975ef982c24a9c
lpintodesa <luis.pintodesa@gmail.com>
2020-05-03 00:27:06 -0500

    Simplified convert()

--- src/main/java/com/luispintodesa/bartender/models/manipulation/SpaceToUnderscoreConverter.java
+++ src/main/java/com/luispintodesa/bartender/models/manipulation/SpaceToUnderscoreConverter.java
@@ -4,16 +4,7 @@ public class SpaceToUnderscoreConverter {
 
     public static String convert(String string){
 
-        StringBuilder processedName = new StringBuilder();
-
-        for (char c: string.toCharArray()){
-            if (Character.isWhitespace(c)){
-                processedName.append("_");
-            } else {
-                processedName.append(c);
-            }
-        }
-        return processedName.toString();
+        return string.replaceAll("\\s", "_");
     }
 
     private SpaceToUnderscoreConverter() {
