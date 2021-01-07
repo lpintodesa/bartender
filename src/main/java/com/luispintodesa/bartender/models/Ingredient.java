79531fbdf0c638233cf7e19f60576d9b49d27dd3
lpintodesa <luis.pintodesa@gmail.com>
2021-01-07 10:04:46 -0600

    Introduced a Many to Many relationship with persistent Drinks in the DB

--- src/main/java/com/luispintodesa/bartender/models/Ingredient.java
+++ src/main/java/com/luispintodesa/bartender/models/Ingredient.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Ingredient {
@@ -22,6 +23,10 @@ public class Ingredient {
 
   @ManyToMany(mappedBy = "ingredients")
   private List<User> users;
+
+  @ManyToMany private List<Drink> drinks;
+
+  public Ingredient() {}
 
   public int getId() {
     return id;
@@ -45,6 +50,26 @@ public class Ingredient {
 
   public void setType(String type) {
     this.type = type;
+  }
+
+  public List<Drink> getDrinks() {
+    return drinks;
+  }
+
+  public void setDrinks(List<Drink> drinks) {
+    this.drinks = drinks;
+  }
+
+  public void addDrink(Drink drink) {
+    this.drinks.add(drink);
+  }
+
+  public void removeDrink(Drink drink) {
+    this.drinks.remove(drink);
+  }
+
+  public List<Integer> getDrinkIds() {
+    return drinks.stream().map(Drink::getId).collect(Collectors.toList());
   }
 
   @Override
