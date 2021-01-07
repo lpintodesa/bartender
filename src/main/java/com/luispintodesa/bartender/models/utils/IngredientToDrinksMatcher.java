91deb0477fe2442f28f90476b7d72c40b159d25b
lpintodesa <luis.pintodesa@gmail.com>
2021-01-07 10:08:33 -0600

    Refactored ingredientMatcher methods

--- src/main/java/com/luispintodesa/bartender/models/utils/IngredientToDrinksMatcher.java
+++ src/main/java/com/luispintodesa/bartender/models/utils/IngredientToDrinksMatcher.java
@@ -1,17 +1,13 @@
 package com.luispintodesa.bartender.models.utils;
 
-import com.google.common.collect.Sets;
 import com.luispintodesa.bartender.models.Drink;
 import com.luispintodesa.bartender.models.Ingredient;
 import com.luispintodesa.bartender.models.User;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
-
-import static com.google.common.collect.Sets.newHashSet;
 
 public class IngredientToDrinksMatcher {
 
@@ -19,43 +15,47 @@ public class IngredientToDrinksMatcher {
 
   public static List<Drink> matchUserIngredientsToDrinks(User theUser, int intendedScore) {
 
-    Set<String> strUserIngredientHashSet = new HashSet<>();
-
-    for (Ingredient ingredient : theUser.getIngredients()) {
-      strUserIngredientHashSet.add(ingredient.getName());
-    }
-
-    return theUser.getIngredients()
-            .stream()
-            .map(DeserializerUtils::searchDrinkIdsBySingleIngredient)
-            .flatMap(Collection::stream)
-            .distinct()
-            .map(DeserializerUtils::searchDrinkById)
-            .filter(drink -> setMatchCounter(drink,strUserIngredientHashSet)==intendedScore)
-            .collect(Collectors.toList());
+    return theUser.getIngredients().stream()
+        .map(Ingredient::getDrinks)
+        .flatMap(Collection::stream)
+        .distinct()
+        .filter(
+            drink ->
+                setMatchCounter(
+                        drink.getLowerCaseIngredientNames(), theUser.getLowerCaseIngredientNames())
+                    == intendedScore)
+        .collect(Collectors.toList());
   }
 
-  public static int setMatchCounter(Drink drink, Set<String> userIngredientsSet) {
+  public static int setMatchCounter(
+      List<String> drinkIngredients, List<String> userIngredientsNames) {
 
-    HashSet<String> strDrinkIngredientsHashSet = newHashSet(
-            drink.getNameIngredient1(),
-            drink.getNameIngredient2(),
-            drink.getNameIngredient3(),
-            drink.getNameIngredient4(),
-            drink.getNameIngredient5(),
-            drink.getNameIngredient6(),
-            drink.getNameIngredient7(),
-            drink.getNameIngredient8(),
-            drink.getNameIngredient9(),
-            drink.getNameIngredient10(),
-            drink.getNameIngredient11(),
-            drink.getNameIngredient12(),
-            drink.getNameIngredient13(),
-            drink.getNameIngredient14(),
-            drink.getNameIngredient15());
+    drinkIngredients.removeAll(userIngredientsNames);
 
-    strDrinkIngredientsHashSet.remove(null);
+    return drinkIngredients.size();
+  }
 
-    return Sets.difference(strDrinkIngredientsHashSet,userIngredientsSet).size();
+  public static void updateIngredientsToDrinksMatch (User theUser) {
+
+    for (Ingredient ingredient : theUser.getIngredients()) {
+      List<Integer> drinkIds = DeserializerUtils.searchDrinkIdsBySingleIngredient(ingredient);
+      List<Integer> newIds =
+              (List<Integer>) CollectionUtils.removeAll(drinkIds, ingredient.getDrinkIds());
+
+      for (Drink drink : ingredient.getDrinks()) {
+        if (!drinkIds.contains(drink.getId())) {
+          ingredient.removeDrink(drink);
+        }
+      }
+
+      for (Integer id : drinkIds) {
+        if (newIds.contains(id)) {
+          Drink newDrink = DeserializerUtils.searchDrinkById(id);
+          drinkDao.save(newDrink);
+          ingredient.addDrink(newDrink);
+        }
+      }
+      ingredientDao.save(ingredient);
+    }
   }
 }
