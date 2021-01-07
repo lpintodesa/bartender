f349fab52bc971ede346b43416b9510e609cb05a
lpintodesa <luis.pintodesa@gmail.com>
2021-01-07 10:06:25 -0600

    Created DrinkDao to persist Drink objects

--- src/main/java/com/luispintodesa/bartender/models/dao/DrinkDao.java
+++ src/main/java/com/luispintodesa/bartender/models/dao/DrinkDao.java
@@ -1,2 +1,11 @@
-package com.luispintodesa.bartender.models.dao;public interface DrinkDao {
-}
+package com.luispintodesa.bartender.models.dao;
+
+import com.luispintodesa.bartender.models.Drink;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface DrinkDao extends CrudRepository<Drink, Integer> {}
