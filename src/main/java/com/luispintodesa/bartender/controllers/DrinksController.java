a1247b794b81acaffe77a0d1234ed874df8320b9
lpintodesa <luis.pintodesa@gmail.com>
2021-01-07 09:59:12 -0600

    Refactored controller to simplify getIngredients and getMeasures

--- src/main/java/com/luispintodesa/bartender/controllers/DrinksController.java
+++ src/main/java/com/luispintodesa/bartender/controllers/DrinksController.java
@@ -1,7 +1,7 @@
 package com.luispintodesa.bartender.controllers;
 
-import com.luispintodesa.bartender.models.utils.DeserializerUtils;
 import com.luispintodesa.bartender.models.Drink;
+import com.luispintodesa.bartender.models.utils.DeserializerUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 
 @Controller
 @RequestMapping("drinks")
@@ -20,45 +19,11 @@ public class DrinksController extends UserController {
 
     Drink drink = DeserializerUtils.searchDrinkById(idDrink);
 
-    ArrayList<String> ingredients = new ArrayList<>();
-
-    ingredients.add(drink.getNameIngredient1());
-    ingredients.add(drink.getNameIngredient2());
-    ingredients.add(drink.getNameIngredient3());
-    ingredients.add(drink.getNameIngredient4());
-    ingredients.add(drink.getNameIngredient5());
-    ingredients.add(drink.getNameIngredient6());
-    ingredients.add(drink.getNameIngredient7());
-    ingredients.add(drink.getNameIngredient8());
-    ingredients.add(drink.getNameIngredient9());
-    ingredients.add(drink.getNameIngredient10());
-    ingredients.add(drink.getNameIngredient11());
-    ingredients.add(drink.getNameIngredient12());
-    ingredients.add(drink.getNameIngredient13());
-    ingredients.add(drink.getNameIngredient14());
-    ingredients.add(drink.getNameIngredient15());
-
-    ArrayList<String> measures = new ArrayList<>();
-
-    measures.add(drink.getMeasureIngredient1());
-    measures.add(drink.getMeasureIngredient2());
-    measures.add(drink.getMeasureIngredient3());
-    measures.add(drink.getMeasureIngredient4());
-    measures.add(drink.getMeasureIngredient5());
-    measures.add(drink.getMeasureIngredient6());
-    measures.add(drink.getMeasureIngredient7());
-    measures.add(drink.getMeasureIngredient8());
-    measures.add(drink.getMeasureIngredient9());
-    measures.add(drink.getMeasureIngredient10());
-    measures.add(drink.getMeasureIngredient11());
-    measures.add(drink.getMeasureIngredient12());
-    measures.add(drink.getMeasureIngredient13());
-    measures.add(drink.getMeasureIngredient14());
-    measures.add(drink.getMeasureIngredient15());
+    drinkDao.save(drink);
 
     model.addAttribute("name", drink.getName());
-    model.addAttribute("ingredients", ingredients);
-    model.addAttribute("measures", measures);
+    model.addAttribute("ingredients", drink.getIngredientNames());
+    model.addAttribute("measures", drink.getIngredientMeasures());
     model.addAttribute("instructions", drink.getInstructions());
     model.addAttribute("image", drink.getThumbnail());
     model.addAttribute("title", drink.getName());
