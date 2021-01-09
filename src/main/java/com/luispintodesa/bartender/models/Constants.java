d7e1f346810d55f02659ffa7540acd2ba6cab6a3
lpintodesa <luis.pintodesa@gmail.com>
2021-01-09 09:27:59 -0600

    Added constants.

--- src/main/java/com/luispintodesa/bartender/models/Constants.java
+++ src/main/java/com/luispintodesa/bartender/models/Constants.java
@@ -3,9 +3,17 @@ package com.luispintodesa.bartender.models;
 public class Constants {
 
   public static final String TITLE = "title";
+  public static final String IMAGE = "image";
+  public static final String INSTRUCTIONS = "instructions";
   public static final String REGISTER = "register";
+  public static final String MEASURES = "measures";
+  public static final String REGISTER_PATH = "/register";
+  public static final String INDEX = "index";
+  public static final String INDEX_PATH = "/index";
   public static final String LOGIN_TITLE = "Log In";
   public static final String LOGIN_TEMPLATE = "login";
+  public static final String LOGIN_PATH = "/login";
+  public static final String LOGOUT_PATH = "/logout";
   public static final String NO_RESULTS = "No Results";
   public static final String NO_RESULTS_TEMPLATE = "noresults";
   public static final String SEARCH_RESULTS = "Search Results";
@@ -13,7 +21,16 @@ public class Constants {
   public static final String INGREDIENTS = "ingredients";
   public static final String MY_BAR = "My Bar";
   public static final String MY_BAR_TEMPLATE = "mybar";
+  public static final String MY_BAR_PATH = "/mybar";
+  public static final String NAME = "name";
+  public static final String REDIRECT_PATH = "redirect:";
+  public static final String USERNAME = "username";
+  public static final String PASSWORD = "password";
   public static final String ERROR = "error";
+  public static final String USER_SESSION_KEY = "user_id";
+  public static final String DETAILS = "details";
+  public static final String DRINKS = "drinks";
+  public static final String EMPTY_STRING = "";
 
   private Constants() {}
 }
