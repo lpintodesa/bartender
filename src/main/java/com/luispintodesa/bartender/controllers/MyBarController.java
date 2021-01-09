a92fc56b64c90d7f8eef2078cde53e8c7133cb9e
lpintodesa <luis.pintodesa@gmail.com>
2021-01-09 09:27:32 -0600

    Refactored to add constants, use the new ingredientsAndDrinksManager and userManager.

--- src/main/java/com/luispintodesa/bartender/controllers/MyBarController.java
+++ src/main/java/com/luispintodesa/bartender/controllers/MyBarController.java
@@ -1,14 +1,10 @@
 package com.luispintodesa.bartender.controllers;
 
-import com.luispintodesa.bartender.models.Drink;
 import com.luispintodesa.bartender.models.Ingredient;
 import com.luispintodesa.bartender.models.IngredientInList;
 import com.luispintodesa.bartender.models.User;
-import com.luispintodesa.bartender.models.dao.DrinkDao;
-import com.luispintodesa.bartender.models.dao.IngredientDao;
 import com.luispintodesa.bartender.models.forms.MyBarForm;
 import com.luispintodesa.bartender.models.utils.DeserializerUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,22 +16,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import static com.luispintodesa.bartender.models.Constants.EMPTY_STRING;
 import static com.luispintodesa.bartender.models.Constants.ERROR;
 import static com.luispintodesa.bartender.models.Constants.INGREDIENTS;
 import static com.luispintodesa.bartender.models.Constants.MY_BAR;
 import static com.luispintodesa.bartender.models.Constants.MY_BAR_TEMPLATE;
+import static com.luispintodesa.bartender.models.Constants.REDIRECT_PATH;
 import static com.luispintodesa.bartender.models.Constants.TITLE;
 
 @Controller
 @RequestMapping(MY_BAR_TEMPLATE)
 public class MyBarController extends UserController {
 
-  @Autowired private IngredientDao ingredientDao;
-  @Autowired private DrinkDao drinkDao;
-
-  @GetMapping(value = "")
+  @GetMapping(value = EMPTY_STRING)
   public String myBarForm(Model model) {
 
     model.addAttribute(INGREDIENTS, DeserializerUtils.listAllIngredients());
@@ -44,13 +38,14 @@ public class MyBarController extends UserController {
     return MY_BAR_TEMPLATE;
   }
 
-  @PostMapping(value = "")
+  @PostMapping(value = EMPTY_STRING)
   public String myBar(Model model, @ModelAttribute MyBarForm form, HttpServletRequest request) {
 
     ArrayList<IngredientInList> list =
         (ArrayList<IngredientInList>) DeserializerUtils.listAllIngredients();
 
     User theUser = getUserFromSession(request.getSession());
+
     List<Ingredient> ingredientsInMyBar = theUser.getIngredients();
 
     if (!form.checkIngredientInList(list)) {
@@ -71,24 +66,11 @@ public class MyBarController extends UserController {
 
     Ingredient newIngredient = DeserializerUtils.searchIngredientByName(form.getIngredientName());
 
-    List<Drink> drinks =
-        DeserializerUtils.searchDrinkIdsBySingleIngredient(newIngredient).stream()
-            .map(
-                id ->
-                    (drinkDao.existsById(id)
-                        ? (drinkDao.findById(id)).get()
-                        : DeserializerUtils.searchDrinkById(id)))
-            .collect(Collectors.toList());
+    ingredientsAndDrinksManager.addNewDrinksForNewIngredient(newIngredient);
 
-    drinkDao.saveAll(drinks);
+    userManager.addIngredientAndSave(theUser, newIngredient);
 
-    newIngredient.setDrinks(drinks);
-    ingredientDao.save(newIngredient);
-
-    theUser.addItem(newIngredient);
-    userDao.save(theUser);
-
-    model.addAttribute("ingredients", DeserializerUtils.listAllIngredients());
+    model.addAttribute(INGREDIENTS, DeserializerUtils.listAllIngredients());
     model.addAttribute(new MyBarForm());
     model.addAttribute(ERROR, "false");
     model.addAttribute(TITLE, MY_BAR);
@@ -97,21 +79,18 @@ public class MyBarController extends UserController {
 
   @PostMapping(value = "remove")
   public String remove(
-      @ModelAttribute @RequestParam(required = false) int[] ingredientIDs,
+      @ModelAttribute @RequestParam(required = false) Integer[] ingredientIDs,
       Model model,
       HttpServletRequest request) {
 
     if (ingredientIDs == null) {
-      return "redirect:./";
+      return REDIRECT_PATH+"./";
     }
 
     User theUser = getUserFromSession(request.getSession());
 
-    for (int ingredientID : ingredientIDs) {
+    userManager.removeIngredientsAndSave(theUser, ingredientIDs);
 
-      theUser.getIngredients().removeIf(ingredient -> ingredient.getId() == ingredientID);
-    }
-    userDao.save(theUser);
-    return "redirect:./";
+    return REDIRECT_PATH+"./";
   }
 }
