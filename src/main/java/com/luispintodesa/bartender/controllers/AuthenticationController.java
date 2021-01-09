a3357a607c76873beaa8f9935da49e89bb0b4881
lpintodesa <luis.pintodesa@gmail.com>
2021-01-09 09:24:34 -0600

    Refactored to add constants, use the new ingredientsAndDrinksManager and added logic to add N/A when measure is missing.

--- src/main/java/com/luispintodesa/bartender/controllers/AuthenticationController.java
+++ src/main/java/com/luispintodesa/bartender/controllers/AuthenticationController.java
@@ -1,12 +1,8 @@
 package com.luispintodesa.bartender.controllers;
 
-import com.luispintodesa.bartender.models.Drink;
-import com.luispintodesa.bartender.models.Ingredient;
 import com.luispintodesa.bartender.models.User;
 import com.luispintodesa.bartender.models.forms.LoginForm;
 import com.luispintodesa.bartender.models.forms.RegisterForm;
-import com.luispintodesa.bartender.models.utils.DeserializerUtils;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,30 +12,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.List;
 
+import static com.luispintodesa.bartender.models.Constants.EMPTY_STRING;
+import static com.luispintodesa.bartender.models.Constants.INDEX;
+import static com.luispintodesa.bartender.models.Constants.LOGIN_PATH;
 import static com.luispintodesa.bartender.models.Constants.LOGIN_TEMPLATE;
 import static com.luispintodesa.bartender.models.Constants.LOGIN_TITLE;
+import static com.luispintodesa.bartender.models.Constants.LOGOUT_PATH;
+import static com.luispintodesa.bartender.models.Constants.MY_BAR_PATH;
+import static com.luispintodesa.bartender.models.Constants.PASSWORD;
+import static com.luispintodesa.bartender.models.Constants.REDIRECT_PATH;
 import static com.luispintodesa.bartender.models.Constants.REGISTER;
+import static com.luispintodesa.bartender.models.Constants.REGISTER_PATH;
 import static com.luispintodesa.bartender.models.Constants.TITLE;
+import static com.luispintodesa.bartender.models.Constants.USERNAME;
 
 @Controller
 public class AuthenticationController extends UserController {
 
-  @GetMapping(value = "")
+  @GetMapping(value = EMPTY_STRING)
   public String index(Model model) {
-    model.addAttribute(TITLE, "The E-Bartender");
-    return "index";
+    model.addAttribute(TITLE, "The Bartender");
+    return INDEX;
   }
 
-  @GetMapping(value = "/register")
+  @GetMapping(value = REGISTER_PATH)
   public String registerForm(Model model) {
     model.addAttribute(new RegisterForm());
     model.addAttribute(TITLE, "Registration");
     return REGISTER;
   }
 
-  @PostMapping(value = "/register")
+  @PostMapping(value = REGISTER_PATH)
   public String register(
       @ModelAttribute @Valid RegisterForm form, Errors errors, HttpServletRequest request) {
 
@@ -47,29 +51,29 @@ public class AuthenticationController extends UserController {
       return REGISTER;
     }
 
-    User existingUser = userDao.findByUsername(form.getUsername());
+    User existingUser = userManager.findUserByUsername(form.getUsername());
 
     if (existingUser != null) {
       errors.rejectValue(
-          "username", "username.alreadyexists", "A user with that username already exists");
+          USERNAME, "username.alreadyexists", "A user with that username already exists");
       return REGISTER;
     }
 
     User newUser = new User(form.getUsername(), form.getPassword());
-    userDao.save(newUser);
+    userManager.saveUser(newUser);
     setUserInSession(request.getSession(), newUser);
 
-    return "redirect:/mybar";
+    return REDIRECT_PATH + MY_BAR_PATH;
   }
 
-  @GetMapping(value = "/login")
+  @GetMapping(value = LOGIN_PATH)
   public String login(Model model) {
     model.addAttribute(new LoginForm());
     model.addAttribute(TITLE, LOGIN_TITLE);
     return LOGIN_TEMPLATE;
   }
 
-  @PostMapping(value = "/login")
+  @PostMapping(value = LOGIN_PATH)
   public String login(
       Model model,
       @ModelAttribute @Valid LoginForm form,
@@ -81,50 +85,31 @@ public class AuthenticationController extends UserController {
       return LOGIN_TEMPLATE;
     }
 
-    User theUser = userDao.findByUsername(form.getUsername());
+    User theUser = userManager.findUserByUsername(form.getUsername());
     String password = form.getPassword();
 
-    if (theUser == null) {
-      errors.rejectValue("username", "user.invalid", "The given username does not exist");
+    if (null == theUser) {
+      errors.rejectValue(USERNAME, "user.invalid", "The given username does not exist");
       model.addAttribute(TITLE, LOGIN_TITLE);
       return LOGIN_TEMPLATE;
     }
 
     if (!theUser.isMatchingPassword(password)) {
-      errors.rejectValue("password", "password.invalid", "Invalid password");
+      errors.rejectValue(PASSWORD, "password.invalid", "Invalid password");
       model.addAttribute(TITLE, LOGIN_TITLE);
       return LOGIN_TEMPLATE;
     }
 
     setUserInSession(request.getSession(), theUser);
 
-    for (Ingredient ingredient : theUser.getIngredients()) {
-      List<Integer> drinkIds = DeserializerUtils.searchDrinkIdsBySingleIngredient(ingredient);
-      List<Integer> newIds =
-          (List<Integer>) CollectionUtils.removeAll(drinkIds, ingredient.getDrinkIds());
+    ingredientsAndDrinksManager.updateDrinksForAllIngredients(theUser);
 
-      for (Drink drink : ingredient.getDrinks()) {
-        if (!drinkIds.contains(drink.getId())) {
-          ingredient.removeDrink(drink);
-        }
-      }
-
-      for (Integer id : drinkIds) {
-        if (newIds.contains(id)) {
-          Drink newDrink = DeserializerUtils.searchDrinkById(id);
-          drinkDao.save(newDrink);
-          ingredient.addDrink(newDrink);
-        }
-      }
-      ingredientDao.save(ingredient);
-    }
-
-    return "redirect:/mybar";
+    return REDIRECT_PATH + MY_BAR_PATH;
   }
 
-  @GetMapping(value = "/logout")
+  @GetMapping(value = LOGOUT_PATH)
   public String logout(HttpServletRequest request) {
     request.getSession().invalidate();
-    return "redirect:";
+    return REDIRECT_PATH;
   }
 }
